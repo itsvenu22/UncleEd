6205a6b027c79225bbc5cf7e48ab4bfe8b9c5b1a
Venukanthan BS <itsvenu22@gmail.com>
2024-10-22 12:56:58 +0530

    base(add review) for each mock, for every test, review adding working viewable in admin panel

--- project_uncleed_site/app_exams/admin.py
+++ project_uncleed_site/app_exams/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 from .models import CustomUser, Exam, MockTest
 
+from django.contrib import admin
+from .models import Review
+
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ['name', 'email', 'mobile_number']
 
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Exam)
 admin.site.register(MockTest)
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('user', 'mock_test', 'get_exam_title', 'created_at')
+
+    def get_exam_title(self, obj):
+        return obj.mock_test.exam.title
+    get_exam_title.short_description = 'Exam Title'
+
+admin.site.register(Review, ReviewAdmin)
