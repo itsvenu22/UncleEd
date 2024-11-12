27015b11ce24a9253f04e30f15b4c479eaa95242
Venukanthan BS <itsvenu22@gmail.com>
2024-11-12 23:42:31 +0530

    updated date in js
    dec 1, 2024

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("Nov 1, 2024 00:00:00").getTime();
+    const countDate = new Date("Dec 1, 2024 00:00:00").getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
